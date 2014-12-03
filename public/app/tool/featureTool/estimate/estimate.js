72d6b2c021abf8f0e0efbb7a98510b3a2e952ab5
dingziran <dingziranrr@163.com>
2014-12-03 22:34:59 +0800

    tmp

---
+++ public/app/tool/featureTool/estimate/estimate.js
@@ -0,0 +1,26 @@
+/**
+ * Created by dingziran on 2014/11/14.
+ */
+var app=angular.module("gcgl2016.template");
+app.config(function($stateProvider) {
+    $stateProvider
+        .state('main.project.activity.featureDefault', {
+            url: "/featureDefault/:featureId",
+            templateUrl: "app/tool/featureTool/featureDefault.html",
+            resolve:{
+                feature:function($stateParams,activityData){
+                    return _.filter(activityData.features,function(feature){
+                        return feature.featureId==$stateParams.featureId;
+                    })[0];
+                }
+            },
+            controller: function ($scope,$stateParams,f,
+                                  activityData,feature) {
+                $scope.activityData= f.copy(activityData);
+                $scope.feature= f.copy(feature);
+            },
+            data: {
+                displayName: 'Feature'
+            }
+        });
+});
